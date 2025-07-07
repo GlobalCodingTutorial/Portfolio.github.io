a50574a5732a36af82f34ac16db605c3abcbb42b
GlobalCodingTutorial <vgola261@gmail.com>
2025-07-07 18:29:12 +0530

    yes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React ,{ useState } from 'react'
 import pic from '../components/Images/Untitled2.png' 
 
 import { AiOutlineMenu } from "react-icons/ai";
