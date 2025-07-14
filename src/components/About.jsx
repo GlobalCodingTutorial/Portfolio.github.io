5b7e7195258ffc9e1d2d7ec0c879d73fc5c5a3f3
Vikas Gola <vgola261@gmail.com>
2025-07-14 21:20:37 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,12 @@ function About() {
     <div>
     <h1 className='text-3xl font-bold mb-5'>About</h1>    
      <p className='text-sm md:text-lg text-justify'>
-       I'm a computer science diploma graduate with a passion for building smart, practical web's. Skilled in Java, PHP, JavaScript, and modern web technologies like React, I thrive on solving problems and learning fast. With a team-first mindset and a drive to grow, I'm ready to take on real-world challenges and create impactful solutions.
+     I am a computer science diploma graduate with a strong passion for software development 
+     and problem-solving. Skilled in programming languages such as Java, Php, 
+     and JavaScript, as well as web development technologies like HTML, CSS, and 
+     React, I am eager to apply my technical knowledge in real-world projects. 
+     A quick learner with a collaborative mindset, I am excited to grow as a 
+     developer and contribute to innovative solutions in a dynamic work environment.
      </p>
      <br />
      {/*Education & Traning*/}
@@ -22,7 +27,7 @@ function About() {
            <tr class="text-slate-500 border-b border-slate-300 bg-slate-50">
         <th class="p-4">
           <p class="text-sm leading-none font-normal">
-            Course 
+            Course
           </p>
         </th>
         <th class="p-4">
@@ -122,7 +127,95 @@ function About() {
                </p>
               </td>
              </tr>
-            
+        
+           </tbody>
+         </table>
+       </div>
+     </span>
+     <br />
+     <br />
+          {/*Professional Experience*/}
+     <h1 className='text-green-600 font-semibold text-xl'>
+      Professional Training & Experience 
+     </h1>
+          <span className='text-sm md:text-lg'>
+       <div class="relative flex flex-col w-full h-full overflow-scroll text-gray-700 bg-white shadow-md rounded-lg bg-clip-border">
+         <table class="w-full text-left table-auto min-w-max text-slate-800">
+           {/*table head start from here */}
+           <thead>
+           <tr class="text-slate-500 border-b border-slate-300 bg-slate-50">
+        <th class="p-4">
+          <p class="text-sm leading-none font-normal">
+           Company
+          </p>
+        </th>
+        <th class="p-4">
+          <p class="text-sm leading-none font-normal">
+            Department 
+          </p>
+        </th>
+        <th class="p-4">
+          <p class="text-sm leading-none font-normal">
+            Desigation
+          </p>
+        </th>
+        <th class="p-4">
+          <p class="text-sm leading-none font-normal">
+            Duration
+          </p>
+        </th>
+           </tr>
+           </thead>
+           {/*table body start from here */}
+           <tbody>
+
+            {/*1*/}
+            <tr class="hover:bg-slate-50">
+        <td class="p-4">
+          <p class="text-sm font-bold">
+            Rajiv Gandhi Cancer Institute and Research Center
+          </p>
+        </td>
+        <td class="p-4">
+          <p class="text-sm">
+           Information Technology
+          </p>
+        </td>
+        <td class="p-4">
+          <p class="text-sm">
+            Web Developer trainee
+          </p>
+        </td>
+        <td class="p-4">
+          <p class="text-sm">
+            Mar/2024 - Jun/2024
+          </p>
+        </td>
+             </tr>
+                {/*2*/}
+             <tr class="hover:bg-slate-50">
+              <td class="p-4">
+          <p class="text-sm font-bold">
+            Fabasian Industrie's Pvt. Ltd. 
+          </p>
+              </td>
+              <td class="p-4">
+          <p class="text-sm">
+            Business Management
+          </p>
+              </td>
+              <td class="p-4">
+               <p class="text-sm">
+                GeM Portal Operator
+               </p>
+              </td>
+              <td class="p-4">
+               <p class="text-sm">
+                Dec/2024 - May/2025
+               </p>
+              </td>
+             </tr>
+      
            </tbody>
          </table>
        </div>
@@ -139,14 +232,14 @@ function About() {
           <b>
            Programming Skills :-
           </b>
-          Frontend(html,css & js) and Backend(Basic Java & php), framework(Bootstrap, Tailwind CSS, Spring-boot), tools(Vscode, Xampp, Apache Tomcat & Mysqli).
+          Frontend(html,css & js) and Backend(Basic Java & php), framework(Bootstrap, Tailwind CSS, Spring-boot), tools(Vscode, Xampp & Mysql).
         </li>
         <br />
         <li>
           <b>
             Techanical Skills :-
           </b>
-          Database management, Computer operator, File management, Project management.
+          Data management, Computer operaton, File management, Web Designing etc.
         </li>
         <br />
         <li>
@@ -159,18 +252,7 @@ function About() {
      </span>
      <br />
      <br />
-     {/*Professional Experience*/}
-     <h1 className='text-green-600 font-semibold text-xl'>
-      Professional Experience
-     </h1>
-     <span className='text-sm md:text-lg'>
-        <h1>
-          <b>Fresher :-</b> 
-          Enthusiastic and ready to take on challenges.
-        </h1>
-     </span>
-     <br />
-     <br />
+
      {/*Achievements & Awards*/}
      <h1 className='text-green-600 font-semibold text-xl'>
       Achievements & Awards
@@ -192,12 +274,13 @@ function About() {
      </span>
      <br />
      <br />
+
      {/*Mission Statement*/}
      <h1 className='text-green-600 font-semibold text-xl'>
       Mission Statement
      </h1>
      <span className='text-sm md:text-lg text-justify'>
-      As a fresher web developer, my mission is to build visually engaging, user-friendly, and     responsive websites by applying modern web technologies and a continuous learning mindset. I am committed to writing clean, maintainable code and delivering solutions that meet both user needs and business goals. With a strong foundation in HTML, CSS, JavaScript, and frameworks like React, I aim to grow into a full-stack developer who contributes to innovative, high-impact web applications. I thrive on challenges and am eager to collaborate, adapt, and evolve in the dynamic world of web development.
+      As a developer, my mission is to build visually engaging, user-friendly, and     responsive websites by applying modern web technologies and a continuous learning mindset. I am committed to writing clean, maintainable code and delivering solutions that meet both user needs and business goals. With a strong foundation in HTML, CSS, JavaScript, and frameworks like React, I aim to grow into a full-stack developer who contributes to innovative, high-impact web applications. I thrive on challenges and am eager to collaborate, adapt, and evolve in the dynamic world of web development.
      </span>
     </div>
     </div>
